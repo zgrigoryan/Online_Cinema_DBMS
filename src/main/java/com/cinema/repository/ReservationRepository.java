6dc2dd9a937f679c0b04388dc253a0e3c90135bb
zgrigoryan <grigoryanzara08@gmail.com>
2025-11-29 22:07:12 +0400

    import springframework jpa repo query

--- src/main/java/com/cinema/repository/ReservationRepository.java
+++ src/main/java/com/cinema/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.cinema.domain.model.Customer;
 import com.cinema.domain.model.Reservation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
   boolean existsByCustomer(Customer customer);
